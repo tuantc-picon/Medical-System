26290c6dd1a6046f316874b1e15397e0f22e2aa7
Tấn Huỳnh Vĩnh <123220176@sv1.dut.udn.vn>
2025-07-26 00:35:47 +0700

    add role to token

--- core/services/authention.py
+++ core/services/authention.py
@@ -4,15 +4,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from core.services import query
 from core.utils.token import create_access_token, create_refresh_token
 
-async def login(db : AsyncSession, request: OAuth2PasswordRequestForm = Depends()):
-    if not query.verify_authention(request.username, request.password, db):
+async def login(db : AsyncSession, request: OAuth2PasswordRequestForm):
+    query_user=await query.verify_authention(request.username, request.password, db)
+    if not query_user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    access_token = create_access_token(data={"sub": request.username})
-    refresh_token = create_refresh_token(data={"sub": request.username})
+    access_token = create_access_token(data={"subEmail": query_user.email,
+                                             "subName": query_user.name,
+                                             "role": query_user.role})
+    refresh_token = create_refresh_token(data={"sub": query_user.username})
     return {"access_token": access_token,
             "refresh_token": refresh_token,
             "token_type": "Bearer"
--- core/services/query.py
+++ core/services/query.py
@@ -5,13 +5,15 @@ from pydantic import EmailStr
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.utils.hashing import Hash
 
-async def verify_db(email:EmailStr, name: str, db: AsyncSession):
+async def verify_db(email:EmailStr, name: str, role:str, db: AsyncSession):
     query = select(User).where(User.email == email)
     result = await db.execute(query)
     user = result.scalar_one_or_none()
     if not user:
         return False
     if user.name != name:
+        return False
+    if user.role != role:
         return False
     return True
 
@@ -20,5 +22,5 @@ async def verify_authention(username,password, db: AsyncSession):
     result = await db.execute(stmt)
     user = result.scalar_one_or_none()
     if not user or not Hash.verify(password, user.password):
-        return False
-    return True
+        return None
+    return user
