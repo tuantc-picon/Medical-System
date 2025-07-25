81aa4f65523270178f147851e82a5cd772e3a832
Tấn Huỳnh Vĩnh <123220176@sv1.dut.udn.vn>
2025-07-26 03:56:20 +0700

    add schedule

---
+++ api/v2/schedule.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Depends, status
+
+from core.common.constants import RoleEnum
+from core.services.schedule import create_schedule
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.users.schemas.schedule import ScheduleDoctorCreate
+from core.common.database import get_async_db_session
+from core.services.role_base import require_role
+
+
+schedule_router = APIRouter(
+    prefix="/v2/schedule",
+    tags=["Schedule"]
+)
+
+@schedule_router.post("/", status_code=status.HTTP_201_CREATED)
+async def createSchedule(schedule: ScheduleDoctorCreate,
+                         db:AsyncSession = Depends(get_async_db_session)):
+    return await create_schedule(schedule, db)
