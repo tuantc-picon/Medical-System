3990bc2aec6f59b0bc0c45d7221b7c24d189e2cd
Tấn Huỳnh Vĩnh <123220176@sv1.dut.udn.vn>
2025-07-26 00:38:26 +0700

    fix file query

--- core/services/query.py
+++ core/services/query.py
@@ -5,7 +5,7 @@ from pydantic import EmailStr
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.utils.hashing import Hash
 
-async def verify_db(email:EmailStr, name: str, role:str, db: AsyncSession):
+async def verify_db(email:EmailStr, name: str, db: AsyncSession):
     query = select(User).where(User.email == email)
     result = await db.execute(query)
     user = result.scalar_one_or_none()
@@ -13,14 +13,12 @@ async def verify_db(email:EmailStr, name: str, role:str, db: AsyncSession):
         return False
     if user.name != name:
         return False
-    if user.role != role:
-        return False
     return True
 
-async def verify_authention(username,password, db: AsyncSession):
+async def verify_authention(username,password, role, db: AsyncSession):
     stmt = select(User).where(User.email == username)
     result = await db.execute(stmt)
     user = result.scalar_one_or_none()
-    if not user or not Hash.verify(password, user.password):
+    if not user or not Hash.verify(password, user.password) or user.role != role:
         return None
     return user
