e8c7eca3fe16e668ecc3b62d9fb3bbade50a7368
Tấn Huỳnh Vĩnh <123220176@sv1.dut.udn.vn>
2025-07-30 09:19:54 +0700

    change api/route.py

--- api/routers.py
+++ api/routers.py
@@ -1,15 +1,9 @@
 from fastapi import APIRouter
-
+from api.v1.users import router as users_v1
 router = APIRouter(redirect_slashes=False)
 
 
-
-
-from api.v1.users import router as users_v1
-
 router.include_router(users_v1)
-
-
 
 
 @router.get("/health")
