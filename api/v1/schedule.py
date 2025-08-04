ce5576621424296df8fd143811d9b8ca93c35f07
Tấn Huỳnh Vĩnh <123220176@sv1.dut.udn.vn>
2025-08-04 11:12:46 +0700

    fix verify_schedule and rename function

--- api/v1/schedule.py
+++ api/v1/schedule.py
@@ -3,18 +3,16 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.users.schemas.schedule import ScheduleDoctorCreate
 from core.common.database import get_async_db_session
-from core.services.oauth2 import bearer_scheme
 from core.services.schedule import Schedule as ScheduleService
 
 schedule = APIRouter(
-    prefix="/v2/schedule",
+    prefix="/v1/schedule",
     tags=["Schedule"]
 )
 
 
 @schedule.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_schedule_doctor(requires: ScheduleDoctorCreate,
-                                 a=Depends(bearer_scheme),
                                  db: AsyncSession = Depends(get_async_db_session)):
-    use = ScheduleService(db)
-    return await use.doctor_for_the_day(requires)
+    services = ScheduleService(db)
+    return await services.create_doctor_schedule(requires)
