29d7a480ec99b124f2fcb4d6db457d0ecc10cf34
Tấn Huỳnh Vĩnh <123220176@sv1.dut.udn.vn>
2025-08-04 11:12:46 +0700

    fix verify_schedule and rename function

--- app/users/schemas/schedule.py
+++ app/users/schemas/schedule.py
@@ -1,6 +1,9 @@
-from core.schemas.base import MSBaseSchema
-from typing import Optional
 from datetime import datetime
+from typing import Optional
+
+from pydantic import model_validator
+
+from core.schemas.base import MSBaseSchema
 
 
 class ScheduleDoctorCreate(MSBaseSchema):
@@ -8,3 +11,11 @@ class ScheduleDoctorCreate(MSBaseSchema):
     start_time: datetime
     end_time: datetime
     note: Optional[str] = None
+
+    @model_validator(mode="before")
+    def validate_time(cls, values: dict):
+        start = values.get('start_time')
+        end = values.get('end_time')
+        if start and end and start >= end:
+            raise ValueError("Start time must be earlier than end time.")
+        return values
