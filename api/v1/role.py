f7a0ad7b161e4c60befb0c2818533aff74ea6056
Tấn Huỳnh Vĩnh <123220176@sv1.dut.udn.vn>
2025-08-05 10:52:03 +0700

    set up permissions
    change RBAC
    
    complete change RBAC
    
    rename and fix logout
    
    fix logout and merge schemas in register
    
    delete Enum and format code
    
    delete Enum and format code
    
    change fix spelling error in file schema.schedule
    
    fix bug in project
    
    delete credentials_exception in file authention.py
    
    change file bearer.py
    
    # Conflicts:
    #	api/v1/__init__.py
    #	api/v1/authentication.py
    #	api/v1/register.py
    #	api/v1/role.py
    #	api/v1/schedule.py
    #	app/users/schemas/token.py
    #	app/users/schemas/user.py
    #	core/models/prescription.py
    #	core/services/authention.py
    #	core/services/token.py
    #	core/utils/authorize.py
    #	migrations/versions/8c4a972a3413_init.py

--- api/v1/role.py
+++ api/v1/role.py
@@ -3,10 +3,8 @@ from app.users.schemas.role import RolePermissionCreateSchema
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.common.database import get_async_db_session
 from core.services.role import RoleService
-from core.utils.authorize import authorize_user
 
 role = APIRouter(
-    prefix="/roles",
     tags=["Role"],
 )
 
@@ -15,7 +13,6 @@ role = APIRouter(
 async def assign_role_permission(
     data: RolePermissionCreateSchema,
     db: AsyncSession = Depends(get_async_db_session),
-    # authorized_user=Depends(authorize_user("permission:create")),
 ):
     service = RoleService(db)
     return await service.assign_role_permission(data)
